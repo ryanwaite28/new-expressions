dbd662736a054d33ae6ce99f3728b082c89b50c6
Ryan Waite <ryanwaite28@gmail.com>
2016-01-26 12:30:13 -0800

    Update. Added Mobile View

---
+++ script.js
@@ -0,0 +1,54 @@
+// JavaScript File
+
+$(document).ready(function(){
+	
+	var menuIcon = $('#menu-icon');
+	
+	var linksList = $('.links-list');
+	
+	var subList = $('.links-list li ul');
+	
+	var sociaList = $('.social-list');
+	
+	var navBar = $('.nav-div');
+	
+	var mainContain = $('.main-contain')
+	
+	$('.present-img').append('<button type="button">Add To Cart</button>');
+	
+	menuIcon.click(function () {
+
+		if(navBar.css("height") != "500px") {
+			navBar.css("height", "500px");
+			mainContain.css("height", "calc(100vh - 500px)");
+			linksList.css({
+				'display': 'block',
+				'overflow-y': 'scroll',
+				'width': '100%',
+				'height': '345px',
+				'border-bottom': '1px solid black',
+			});
+			subList.css("display", "block");
+			sociaList.css("display", "block");
+		}
+		else {
+			navBar.css("height", "55px");
+			mainContain.css("height", "calc(100vh - 55px)");
+			linksList.css("display", "none");
+			subList.css("display", "none");
+			sociaList.css("display", "none");
+		}
+		
+	});
+	
+});
+
+function addBtn() {
+	console.log('Added Button');
+	
+	var btn = document.createElement('button');
+	
+	$('.present-img').append('The');
+}
+
+addBtn();
